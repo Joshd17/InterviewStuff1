ab2dfed2fe1dfe99afaf3fb4d1ca27a4971524ee
joshua dunkley <joshd10@hotmail.co.uk>
2022-11-25 00:40:12 +0000

    cleanup for easier use

--- InterviewStuff1/ClientApp/src/AppRoutes.js
+++ InterviewStuff1/ClientApp/src/AppRoutes.js
@@ -1,25 +1,9 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
 import { default as GetTemp } from "./components/GetTemp";
-import { Home } from "./components/Home";
 
 const AppRoutes = [
     {
         element: <GetTemp />,
-        index: true,
-
-    },
-    {
-        path: '/counter',
-        element: <Counter />
-    },
-    {
-        path: '/fetch-data',
-        element: <FetchData />
-    },
-    {
-        path: '/get-temp',
-        element: <GetTemp />
+        index: true
     }
 ];
 
